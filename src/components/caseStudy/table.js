688f651f9baaa4b35109f723e9267fd1a50206bf
traxx10 <chuksben16@gmail.com>
2020-03-24 13:24:33 +0100

    added why helium

--- src/components/caseStudy/table.js
+++ src/components/caseStudy/table.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Img from 'react-image'
 import styles from './table.module.scss'
 
 function Table(props) {
@@ -25,55 +26,170 @@ function Table(props) {
         <div className={styles.text}>
           <p> Ability to pay based on device usage </p>
         </div>
-        <div className={styles.helium}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.helium}>
+          <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
       </div>
       <div className={styles.transparent}>
         <div className={styles.text}>
           <p> Connectivity without sim cards or related fees </p>
         </div>
-        <div className={styles.helium}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.helium}>
+          <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
       </div>
       <div className={styles.fill}>
         <div className={styles.text}>
           <p> Ability to pool data across devices</p>
         </div>
-        <div className={styles.helium}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.helium}>
+          <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/tickBlack.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Question_mark.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
       </div>
       <div className={styles.transparent}>
         <div className={styles.text}>
           <p> Unlimited data usage (no caps) </p>
         </div>
-        <div className={styles.helium}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.helium}>
+          <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/TickDark.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
       </div>
       <div className={styles.fill}>
         <div className={styles.text}>
           <p> Charges for data overage </p>
         </div>
-        <div className={styles.helium}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.helium}>
+          <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/tickBlack.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
+        <div className={styles.icons}>
+          <Img
+            src={require('../../assets/N-A.svg')}
+            className={styles.iconSvg}
+          />
+        </div>
       </div>
       <div className={styles.transparentBottom}>
         <div className={styles.text}>
           <p> </p>
         </div>
         <div className={styles.heliumBottom}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
-        <div className={styles.icons}></div>
+        <div className={styles.icons}>
+          {/* <Img
+            src={require('../../assets/Cross.svg')}
+            className={styles.iconSvg}
+          /> */}
+        </div>
+        <div className={styles.icons}>
+          {/* <Img
+            src={require('../../assets/Tick.svg')}
+            className={styles.iconSvg}
+          /> */}
+        </div>
+        <div className={styles.icons}>
+          {/* <Img
+            src={require('../../assets/N-A.svg')}
+            className={styles.iconSvg}
+          /> */}
+        </div>
       </div>
     </div>
   )
