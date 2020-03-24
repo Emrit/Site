4fd40a5e8be57afde7f3b4c332d845fd198ce3bc
traxx10 <chuksben16@gmail.com>
2020-03-24 16:20:02 +0100

    about add footer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,27 +7,45 @@ import { Container, Row, Col, Button } from 'react-bootstrap'
 function Header(props) {
   return (
     <div className={styles.container}>
-      <Container className={styles.fluidContainer} fluid>
-        <Row className={styles.row}>
-          <Col xs={12} md={12} lg={6} className={styles.col}>
-            <div className={styles.leftContainer}>
-              <h2> Emrit’s Helium Host Program</h2>
-              <p> Come join us now and claim your FREE hotspot!</p>
-              <Button className={styles.button}> More Info </Button>
-            </div>
-          </Col>
-          <Col xs={12} md={12} lg={6} className={styles.col}>
-            <div className={styles.rightContainer}>
-              <Img
-                src={require('../../assets/header/Emrits_White.svg')}
-                className={styles.image}
-              />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div className={styles.leftContainer}>
+        <h2> Emrit’s Helium Host Program</h2>
+        <p> Come join us now and claim your FREE hotspot!</p>
+        <Button className={styles.button}> More Info </Button>
+      </div>
+      <div className={styles.rightContainer}>
+        <Img
+          src={require('../../assets/header/Emrits_White.svg')}
+          className={styles.image}
+        />
+      </div>
     </div>
   )
 }
+
+// function Header(props) {
+//   return (
+//     <div className={styles.container}>
+//       <Container className={styles.fluidContainer} fluid>
+//         <Row className={styles.row}>
+//           <Col xs={12} md={12} lg={6} className={styles.col}>
+//             <div className={styles.leftContainer}>
+//               <h2> Emrit’s Helium Host Program</h2>
+//               <p> Come join us now and claim your FREE hotspot!</p>
+//               <Button className={styles.button}> More Info </Button>
+//             </div>
+//           </Col>
+//           <Col xs={12} md={12} lg={6} className={styles.col}>
+//             <div className={styles.rightContainer}>
+//               <Img
+//                 src={require('../../assets/header/Emrits_White.svg')}
+//                 className={styles.image}
+//               />
+//             </div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   )
+// }
 
 export default Header
