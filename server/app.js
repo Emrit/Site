040c94af077d0914d4e02ba3803887e7604c38a1
Kevin Wang <kw142143@gmail.com>
2020-03-30 17:02:31 -0400

    other fixes and env dev dependency

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
-const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
+require('dotenv').config();
 
 const app = require('./app');
 
